7b9909be2f69d4fa8d9df5635305e9f99fbcafca
SergioFontanLlamas <sfontan@profesores.imf.com>
2023-02-14 13:46:40 +0100

    Clase 14-FEB v2

--- Clases/2023_02_14-JS DOM/app.js
+++ Clases/2023_02_14-JS DOM/app.js
@@ -19,7 +19,6 @@
  * Guardo en la variable 'main' todas las etiquetas de mi HTML que sean <main>
  */
 let main = document.getElementsByTagName('main')[0];
-console.log(main);
 
 /**
  * CREAR UN ELEMENTO SIMPLE.
@@ -57,10 +56,71 @@ ul.appendChild(li);
 main.appendChild(ul);
 
 
+/**
+ * Ejercicio crear formulario desde JavaScript
+ * 
+ * -    etiqueta <form> de tipo 'POST'
+ * -    tres inputs con tipo 'text', 'number' y 'password'
+ * -    uno de los inputs contiene valor y otro 'required'
+ * -    boton de tipo 'button'
+ */
 
+let form, input, button;
 
+form = document.createElement('form');
+form.setAttribute('method', 'POST');
 
+// texto
+input = document.createElement('input');
+input.setAttribute('type', 'text');
+input.setAttribute('required', '');
+form.appendChild(input);
+
+// password
+input = document.createElement('input');
+input.setAttribute('type', 'password');
+form.appendChild(input);
+
+// number
+input = document.createElement('input');
+input.setAttribute('type', 'number');
+input.setAttribute('value', '5');
+form.appendChild(input);
+
+// button
+button = document.createElement('button');
+button.setAttribute('type', 'button');
+button.textContent = 'Enviar';
+form.appendChild(button);
+
+main.appendChild(form);
 
 /**
- * Ejercicio crear formulario
+ * RECOGER VALORES DE UN FORMULARIO
+ * En funcion del valor, aplicar un estilo u otro
+ */
+
+form = document.getElementsByTagName('form')[0];
+input = form.getElementsByTagName('input')[0];
+console.log(typeof input.value);
+if(parseInt(input.value) < 16 && input.value.length < 2){
+    input.style.backgroundColor = 'red';
+    let p = document.createElement('p');
+    p.textContent = 'Te has equivocado al escribir el numero';
+    form.appendChild(p);
+    // Colocar el color rojo en el input
+}else{
+    input.style.backgroundColor = 'green';
+    // acceder a esa etiqueta <p> del mensaje de error y eliminarla si existe
+    // p.remove();
+    // Colocar el color verde en el input
+}
+
+/**
+ * Practica.
+ * 
+ * Crear un flexbox dentro del 'header' que pinte tantos divs como pokemons existan
+ * 
+ * -    Cada div debe contener el nombre de cada pokemon
+ * -    Cada div debe tener un borde para visualizar su contenido
  */
